f001a36fb8800d534705901b7ea09b9baecddcc5
Olaoluwavincent <Olaoluwavincent@gmail.com>
2024-06-26 23:48:50 +0100

    prepping to deploy vercel

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from '@prisma/client';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const prisma = new PrismaClient();
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_KEY!;
+const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
 
 export default prisma;
